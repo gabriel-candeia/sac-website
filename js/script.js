4f23a44c481700ff76732702401ac78c74530af8
Gabriel Camurça <gabriel_cfs@hotmail.com>
2019-08-04 16:45:57 -0300

    Sections 'Palestras' and 'Workshops' filled with proper informaation

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ let c1 = document.getElementById('c1');
 let c2 = document.getElementById('c2');
 let c3 = document.getElementById('c3');
 let c4 = document.getElementById('c4');
-let title = document.getElementById('title')
-let r1 = document.getElementById('r1')
+let c5 = document.getElementById('c5');
+let title = document.getElementById('title');
+let r1 = document.getElementById('r1');
 
-let lista_item_selected = 0
+
+let lista_item_selected = 0;
 
 
 navBarToggle.addEventListener('click', function () {    
@@ -20,24 +22,30 @@ c1.addEventListener('mouseover', show1);
 c2.addEventListener('mouseover', show2);
 c3.addEventListener('mouseover', show3);
 c4.addEventListener('mouseover', show4);
+c5.addEventListener('mouseover', show5);
 
 
 function show1() {
-	ws.innerHTML = "Promote student learning in your math, science, and engineering lab settings from day one. This workshop identifies goals for lab instruction, ways to plan and facilitate effective lab sessions, and strategies for a successful first day of lab class.";
-	title.innerHTML = "Teaching in Lab Settings: First Day and Beyond";
+	ws.innerHTML = "Nesse workshop, será abordado como as novas tendências do desenvolvimento web compõem o React, e como e porquê este se tornou tão conhecido e utilizado pelas grandes empresas (Facebook, Netflix, Microsoft). Além disso, serão abordadas tecnologias como Webpack, Browserify, Node.js e Npm/Yarn.";
+	title.innerHTML = "Introdução a ReactJS";
 }
 
 function show2() {
-	ws.innerHTML = "Which teaching methods do UW students find helpful for learning languages? This workshop explores approaches to common language learning challenges and offers strategies for helping all students meet your course’s language learning goals.";
-	title.innerHTML = "Teaching Modern Languages: First Day and Beyond";
+	ws.innerHTML = "Internet das coisas é um conceito que se refere à interconexão digital de objetos cotidianos com a internet. Durante o workshop, serão feitas atividades com NodeMCU como servidor, gerando interfaces por meio de diferentes protocolos de comunicação.";
+	title.innerHTML = "Internet of Things";
 }
 
 function show3() {
-	ws.innerHTML = "One-to-one teaching settings offer unique opportunities for student learning. This workshop offers strategies to help advance all students’ learning in office hours and study centers.";
-	title.innerHTML = "Teaching 1:1 in Office Hours and Study Centers";
+	ws.innerHTML = "Internet das coisas é um conceito que se refere à interconexão digital de objetos cotidianos com a internet. Durante o workshop, serão feitas atividades com NodeMCU como servidor, gerando interfaces por meio de diferentes protocolos de comunicação.";
+	title.innerHTML = "Internet of Things";
 }
 
 function show4() {
-	ws.innerHTML = "During *social science and humanities (SS/HUM) “quiz” sections, TAs are typically responsible for helping students understand and apply concepts learned in large lecture classes. This workshop includes an overview of strategies for helping all students learn in discussion-based quiz sections."
-	title.innerHTML = "Teaching SS/HUM* Quiz Sections: First Day and Beyond";
+	ws.innerHTML = "GeoPandas é um projeto open source para facilitar a manipulação de dados geoespaciais em Python. Durante o curso, serão feitas diversas atividades relaciondas a sistemas geográficos, tais como: manipular dados georreferenciados, carregar mapas em estruturas de grafos e calcular o menor caminho numa rede de ruas."
+	title.innerHTML = "Sistema de Informação Geográfica em Python com GeoPandas";
+}
+
+function show5() {
+	ws.innerHTML = "O minicurso visa introduzir os alunos ao fantástico mundo do Machine Learning, por meio da formação de uma base sólida e necessária para melhor entendimento da área."
+	title.innerHTML = "Firmando os Pés no Novo Mundo Chamado Machine Learning";
 }
